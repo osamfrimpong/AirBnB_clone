32c9a41e725c8c78617c3e07d2c7f17121f0c205
Osam-Frimpong Schandorf <osamfrimpong@yahoo.com>
2023-11-10 06:15:56 +0000

    FileStorage

---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import json
+
+class FileStorage:
+
+    __file_path = "data_store.json"
+    __objects = {}
+
+    list_of_classes = {}
+
+    def all(self):
+        return self.__objects
+    
+    def new(self, obj):
+        object_key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[object_key] = obj
+
+    def save(self):
+        objects_serialized = {key: object.to_dict() for key, object in self.__objects.items()}
+        with open(self.__file_path, 'w') as database_file:
+            json.dump(objects_serialized, database_file)
+            
+
+    def reload(self):
+        try:
+            with open(self.__file_path, 'r') as database_file:
+                json_string = json.load(database_file)
+                for object_content in json_string.values():
+                    name_of_object_class = object_content["__class__"]
+                    object_class = self.list_of_classes[name_of_object_class]
+
+                    self.new(object_class(**object_content))
+
+        except FileNotFoundError:
+            pass
